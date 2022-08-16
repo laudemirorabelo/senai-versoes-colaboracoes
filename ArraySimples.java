e5acffbd93223ebaabca9bf4864b3d85bfd6b85c
Laudemiro Rabelo <laudemirorabeloneto@gmail.com>
2022-08-16 20:54:53 -0300

    Classe para aprender sobre Arrays em Java

---
+++ ArraySimples.java
@@ -0,0 +1,7 @@
+public class ArraySimples {
+   public static void main(String[] args){
+	String[] paises = {"BRASIL", "RUSSIA", "CHINA", "INDIA", "AFRICA DO SUL"};
+	System.out.println(paises[0]);
+
+   }
+}
